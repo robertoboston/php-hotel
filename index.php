6dda1c06f2b2c3033a47c5082d8a3afcb0745e78
roberto.boston <roberto.bozzo97@tiscali.it>
2023-01-30 16:02:50 +0100

    stampato l'array in pagina

--- index.php
+++ index.php
@@ -40,6 +40,24 @@ $hotels = [
 
 ];
 
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    foreach ($hotels as $item){
+        foreach ($item as $thing){
+            echo $thing."<br>";
+        }
+    }
+    ?>
+    
+</body>
+</html>
